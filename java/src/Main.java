827bb4c124dfb1c0deb21ebeb7fa923d3b15c261
dynamicallocation <ikavanagh@luc.edu>
2018-09-14 04:48:55 +0000

    added new queue for frequency

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-
+import java.io.IOException;
 
 public class Main{
     
@@ -12,56 +12,88 @@ public class Main{
         //private Map<String,Integer> frequency = new HashMap<String,Integer>();
         
         
+       // InputStreamReader in= new InputStreamReader(System.in);
+        //pulling data from input stream
+       // BufferedReader input = new BufferedReader(in);
+        //buffer
+        String ln = null;
         File file = new File("testing.txt");
-        Scanner sc = new Scanner(file);
+        Scanner sc = new Scanner(file).useDelimiter("[.,:;()?!\"\\s]+");
+        //instantiate scanner the goes through file
         Queue<String> q = new LinkedList<String>(); 
+        Queue<Map<String,Integer>> fq = new LinkedList<Map<String,Integer>>();
+        //creat queue
         Map<String,Integer>frequency = new HashMap<String,Integer>();
-        StringBuilder sb = new StringBuilder();
-        int theFrequency = 0;
+        //create hashmap
         int count = 0;
-        String wordFreq;
-        int arg = Integer.parseInt(args[0]);
-        //System.out.println(arg);
-        while(sc.hasNext() && count < arg)
+        //take count of queue
+        String freq;
+        //take frequency of queue
+        
+        //CLA 
+        int howMany = Integer.parseInt(args[0]);
+        //Words that should be displayed
+        int lengthWord = Integer.parseInt(args[1]);
+        //Length of words to be considered valid
+        int lastnWords = Integer.parseInt(args[2]);
+        //Threshhold for moving window
+    
+        while(sc.hasNext())
         {
-            String word = sc.next().replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-            count++;
-            if(word.length() >= Integer.parseInt(args[1]))
+            //String[] words = ln.split(" ");
+            String word = sc.next();
+            //Take words from file while there is words to take
+            if(word.length() >= lengthWord)//Verify valid length
             {
+                
                 q.add(word);
+                //add valid word 
+                
+                if(q.size() >= lastnWords)
+                {
+                    
+                      freq = q.remove();
+                     //Remove the word from back
+                     
+                }
+                else
+                {
+                   
+                    freq = q.peek();
+                    frequency.put(freq,frequency.getOrDefault(freq,0)+1);
+                    fq.add(frequency);
+
+                }
             }
-            
-            
-        }
+            count++;
+            //last n words
+             
+    }
+
+  
+        Map<String,Integer> unsortedMap = new HashMap();
         
-        
-        
-        
-        while(!q.isEmpty())
+        for(int i = 0; i < howMany;i++)
         {
-            
-            String freq = q.remove();
-            frequency.put(freq,frequency.getOrDefault(freq,0)+1);
+            System.out.println(fq.remove());
         }
         
-       /*() MyComparator comp = new MyComparator(frequency);
-        Map<String,Integer> sortedFrequency = new TreeMap(comp);
-        sortedFrequency.putAll(frequency);
-        System.out.println(frequency);
-        Set<String> keys = sortedFrequency.keySet();
-        System.out.println(keys);*/
         
-        //LinkedHashMap preserve the ordering of elements in which they are inserted
+           //LinkedHashMap preserve the ordering of elements in which they are inserted
         LinkedHashMap<String, Integer> reverseSortedMap = new LinkedHashMap<>();
  
-        //Use Comparator.reverseOrder() for reverse ordering
-        frequency.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
- 
-        Set<String> keys = reverseSortedMap.keySet();
-        System.out.println(keys);
+         //Use Comparator.reverseOrder() for reverse ordering
+        unsortedMap.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())).forEachOrdered(x -> reverseSortedMap.put(x.getKey(), x.getValue()));
+       
+        System.out.println(reverseSortedMap);
+        
+        
 
-       //Compare Value implementation by Lokesh Gupta 
-       //https://howtodoinjava.com/sort/java-sort-map-by-values/
+        
+        
+        
+        
+        
        
        
        
